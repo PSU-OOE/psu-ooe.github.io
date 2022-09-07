1fc0cb18511f3a3de014e21182d94b15b4c6fc62
Luke Leber <lal65@psu.edu>
2022-09-07 11:19:50 -0400

    Get something working for Matt

--- alter-twig.php
+++ alter-twig.php
@@ -3,8 +3,21 @@
 use Twig\Environment;
 use Twig\TwigFunction;
 
-
 function addCustomExtension(Environment $env) {
+    $env->addFunction(new TwigFunction('get_component_stylesheets', function () {
+        $styles = [];
+        foreach (glob('patterns/**/*/dist/styles.css') as $component) {
+            $styles[] = str_replace('patterns', 'css', $component);
+        }
+        return $styles;
+    }));
 
+  $env->addFunction(new TwigFunction('get_component_scripts', function () {
+    $scripts = [];
+    foreach (glob('patterns/**/*/dist/scripts.js') as $component) {
+      $scripts[] = str_replace('patterns', 'js', $component);
+    }
+    return $scripts;
+  }));
 }
 
---
+++ scripts/postinstall.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
+
+const components = fs.readdirSync('node_modules/@psu-ooe/');
+components.forEach(component => {
+  const manifest = require(path.join('../', 'node_modules', '@psu-ooe', component, 'package.json'));
+  const namespace = manifest.ooe.namespace;
+  fse.copy(
+      path.join('node_modules', '@psu-ooe', component),
+      path.join('patterns', namespace, component),
+      { recursive: true, overwrite: true }
+  ).then(() => {
+    fse.copySync(
+        path.join('patterns', 'global', 'webfonts', 'webfonts'),
+        path.join('source', 'webfonts'),
+        { recursive: true, overwrite: true }
+    );
+  }).then(() => {
+    const source_dir = path.join('source', 'js');
+    if (!fse.existsSync(source_dir)) {
+      fse.mkdirSync(source_dir);
+    }
+    const script = path.join('patterns', namespace, component, 'dist', 'scripts.js');
+    if (fse.existsSync(script)) {
+      fse.copySync(
+          script,
+          path.join('source', 'js', namespace, component, 'dist', 'scripts.js'),
+          { overwrite: true }
+      );
+    }
+  }).then(() => {
+    const source_dir = path.join('source', 'css');
+    if (!fse.existsSync(source_dir)) {
+      fse.mkdirSync(source_dir);
+    }
+    const script = path.join('patterns', namespace, component, 'dist', 'styles.css');
+    if (fse.existsSync(script)) {
+      fse.copySync(
+          script,
+          path.join('source', 'css', namespace, component, 'dist', 'styles.css'),
+          { overwrite: true }
+      );
+    }
+  });
+
+});
